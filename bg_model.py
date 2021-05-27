1e69628df851919577f7083181de46f569d5a644
mariiapopova <mari.mashapopova@mail.ru>
2021-05-27 14:56:12 +0200

    added interneurons

--- bg_model.py
+++ bg_model.py
@@ -7,7 +7,7 @@ import os
 
 os.chdir(r"C:\Users\Maria\Desktop\PhD\Code\NetPyNE_course\model")
 
-!mknrndll
+!nrnivmodl
 
 #%% Network parameters
 netParams = specs.NetParams()  # object of class NetParams to store the network parameters
@@ -48,7 +48,6 @@ netParams.cellParams['GPe'] = {
     }
 }
 
-# check ampa and gaba
 netParams.cellParams['STN'] = {
     "conds": {},
     "secs": {
@@ -81,6 +80,7 @@ netParams.cellParams['Th'] = {
     "conds": {},
     "secs": {
         "soma": {
+            'threshold': -10,
             "geom": {
                 "nseg": 1,
                 "diam": 100,
@@ -92,6 +92,38 @@ netParams.cellParams['Th'] = {
                 'thalamic_i_leak': {},
                 "thalamic_i_na_k": {},
                 "thalamic_i_t": {}
+            },
+            "pointps": {
+                'SN': {
+                 'mod': 'SynNoise',
+                 'f0': 0,
+                 'f1': 0.3}
+            }
+        }
+    }
+}
+
+netParams.cellParams['Inter'] = {
+    "conds": {},
+    "secs": {
+        "soma": {
+            "geom": {
+                "nseg": 1,
+                "diam": 25,
+                "L": 35,
+                "Ra": 150.0,
+                "cm": 1
+            },
+            "mechs": {
+                'interneuron_i_leak': {},
+                "interneuron_i_na": {},
+                "interneuron_i_k": {}
+            },
+            "pointps": {
+                'SN': {
+                 'mod': 'SynNoise',
+                 'f0': 0,
+                 'f1': 0.3}
             }
         }
     }
@@ -175,11 +207,29 @@ netParams.popParams['Th_pop'] = {
     ]
 }
 
-#check!!
+netParams.popParams['Inter_pop'] = {
+    "cellModel": "",
+    "cellType": 'Inter',
+    "numCells": pop_Size
+    # "yRange": [
+    #     0,
+    #     500
+    # ],
+    # "xRange": [
+    #     500,
+    #     1000
+    # ],
+    # "zRange": [
+    #     0,
+    #     100
+    # ]
+}
+
+#check!! - modify upon healthy state
 netParams.popParams['Str_pop'] = {
     "cellModel": "",
     "cellType": 'Str',
-    'rate': 3,
+    'rate': 5,
     'noise': 0, #check if needed
     'start': 1, #check if needed
     "numCells": pop_Size
@@ -187,26 +237,44 @@ netParams.popParams['Str_pop'] = {
 
 #%% Add stimulus
 #check out amplitude again!!
-netParams.stimSourceParams['bias_gpe'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': -0.009}
-netParams.stimSourceParams['bias_gpi'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.356} #0.006
-netParams.stimSourceParams['bias_stn'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.6} #-0.125
+netParams.stimSourceParams['bias_gpe'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.04} #-0.009
+netParams.stimSourceParams['bias_gpi'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.33} #0.006 #0.356
+netParams.stimSourceParams['bias_stn'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0} #-0.125 #0.6
+netParams.stimSourceParams['bias_inter'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.070} 
 #no bias current for thalamus in original paper
+# netParams.stimSourceParams['bkg']={
+#     'type': 'NetStim',
+#     'rate': 12,
+#     'noise': 0.2
+# }
 
 #%% Add target
 netParams.stimTargetParams['bias_gpe->gpe'] = {'source': 'bias_gpe','sec':'soma', 'loc': 0.5, 'conds': {'pop':'GPe_pop'}}
 netParams.stimTargetParams['bias_gpi->gpi'] = {'source': 'bias_gpi','sec':'soma', 'loc': 0.5, 'conds': {'pop':'GPi_pop'}}
 netParams.stimTargetParams['bias_stn->stn'] = {'source': 'bias_stn','sec':'soma', 'loc': 0.5, 'conds': {'pop':'STN_pop'}}
+netParams.stimTargetParams['bias_inter->inter'] = {'source': 'bias_inter','sec':'soma', 'loc': 0.5, 'conds': {'pop':'Inter_pop'}}
 
 #%% Synaptic mechanism parameters
 netParams.synMechParams['AMPA'] = {'mod': 'AMPA_S'}  # excitatory synaptic mechanism
 netParams.synMechParams['GABA'] = {'mod': 'GABAa_S'}  # inhibitory synaptic mechanism
 
+# netParams.stimTargetParams['bkg->Th'] = {
+#     'source': 'bkg',
+#     'conds': {'cellType': 'Th'},
+#     'weight': 1,
+#     'sec': 'soma',
+#     'loc': 0.5,
+#     'delay': 5,
+#     'synMech': 'GABA'
+# }
 #%% Connections
 netParams.connParams['STN->GPe'] = {
     'preConds': {'pop': 'STN_pop'}, 
     'postConds': {'pop': 'GPe_pop'},
     'weight': 0.111111,
     'convergence': 1,
+    'sec': 'soma',
+    'loc': 0.5,
     'synMech': 'AMPA',
     'delay': 4}
 
@@ -214,6 +282,8 @@ netParams.connParams['STN->GPi'] = {
     'preConds': {'pop': 'STN_pop'}, 
     'postConds': {'pop': 'GPi_pop'},
     'weight': 0.111111,
+    'sec': 'soma',
+    'loc': 0.5,
     'convergence': 1,
     'synMech': 'AMPA',
     'delay': 2}
@@ -222,6 +292,8 @@ netParams.connParams['GPi->Th'] = {
     'preConds': {'pop': 'GPi_pop'}, 
     'postConds': {'pop': 'Th_pop'},
     'weight': 3,
+    'sec': 'soma',
+    'loc': 0.5,
     'convergence': 1,
     'synMech': 'GABA',
     'delay': 2}
@@ -231,6 +303,8 @@ netParams.connParams['GPe->GPi'] = {
     'postConds': {'pop': 'GPi_pop'},
     'weight': 0.111111,
     'convergence': 1,
+    'sec': 'soma',
+    'loc': 0.5,
     'synMech': 'GABA',
     'delay': 2}
 
@@ -239,39 +313,45 @@ netParams.connParams['GPe->STN'] = {
     'postConds': {'pop': 'STN_pop'},
     'weight': 0.111111,
     'convergence': 2,
+    'sec': 'soma',
+    'loc': 0.5,
     'synMech': 'GABA',
     'delay': 3}
 
 netParams.connParams['GPe->GPe'] = {
     'preConds': {'pop': 'GPe_pop'}, 
     'postConds': {'pop': 'GPe_pop'},
-    'weight': 0.015,
+    'weight': 0.005, #0.015
+    'sec': 'soma',
+    'loc': 0.5,
     'convergence': 1,
     'synMech': 'GABA',
     'delay': 4}
 
-netParams.connParams['Str->GPe'] = {
-    'preConds': {'pop': 'Str_pop'}, 
-    'postConds': {'pop': 'GPe_pop'},
-    'weight': 0.01,
-    'convergence': 1,
-    'synMech': 'GABA',
-    'delay': 1}
+# netParams.connParams['Str->GPe'] = {
+#     'preConds': {'pop': 'Str_pop'}, 
+#     'postConds': {'pop': 'GPe_pop'},
+#     'weight': 0.01,
+#     'convergence': 1,
+#     'sec': 'soma',
+#     'loc': 0.5,
+#     'synMech': 'GABA',
+#     'delay': 1}
 
 #%% cfg  
 cfg = specs.SimConfig()					            # object of class SimConfig to store simulation configuration
 cfg.duration = 1*1e3 						            # Duration of the simulation, in ms
 cfg.dt = 0.01								                # Internal integration timestep to use
-cfg.verbose = 0							                # Show detailed messages 
+cfg.verbose = 0						                # Show detailed messages 
 cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
 cfg.recordStep = 0.01 			
 cfg.filename = 'model_output'  			# Set file output name
 cfg.saveJson = False
-cfg.analysis['plotTraces'] = {'include': [0,pop_Size,2*pop_Size, 3*pop_Size]} # Plot recorded traces for this list of cells
+cfg.analysis['plotTraces'] = {'include': [0,pop_Size,2*pop_Size, 3*pop_Size, 4*pop_Size]} # Plot recorded traces for this list of cells
 cfg.hParams['celsius'] = 36
 cfg.hParams['v_init'] = -68
 
 #%% run
 sim.createSimulateAnalyze(netParams = netParams, simConfig = cfg)
-sim.analysis.plot2Dnet(include = ['GPe_pop','STN_pop', 'GPi_pop', 'Th_pop']);
+sim.analysis.plot2Dnet(include = ['GPe_pop','STN_pop', 'GPi_pop', 'Th_pop', 'Inter_pop']);
 sim.analysis.plotSpikeStats();
