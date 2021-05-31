3310ed9b132eb7e5c142ee2b46940308256dfcb9
mariiapopova <mari.mashapopova@mail.ru>
2021-05-31 12:51:31 +0200

    added cortical pyramidal neuron

--- bg_model.py
+++ bg_model.py
@@ -133,8 +133,37 @@ netParams.cellParams['Inter'] = {
 netParams.cellParams['Str'] = {
     'cellModel': 'NetStim'}
 
+#%% cortex cells - can change to any other model
+netParams.importCellParams(
+    label='pyr',
+    conds={'cellType': 'pyr_type', 'cellModel': 'pyr_model'},
+    fileName='c91662.CNG.swc', 
+    cellName='celldef',
+    importSynMechs=False);
+    
+netParams.renameCellParamsSec('pyr', 'soma_0', 'soma')  # rename imported section 'soma_0' to 'soma'
+
+#netParams.cellParams['pyr']['secs']['soma']['geom']['Ra'] = 150 #if want same Ra everywhere
+netParams.cellParams['pyr']['secs']['soma']['mechs']['hh'] = {}
+#if want mechanisms like in fleming
+# netParams.cellParams['pyr']['secs']['soma']['mechs']['cortical_soma_i_leak'] = {}
+# netParams.cellParams['pyr']['secs']['soma']['mechs']['cortical_soma_i_na'] = {}
+# netParams.cellParams['pyr']['secs']['soma']['mechs']['cortical_soma_i_k'] = {}
+# netParams.cellParams['pyr']['secs']['soma']['mechs']['cortical_soma_i_m'] = {}
+netParams.cellParams['pyr']['secs']['soma']['pointps']['SN'] = {'mod': 'SynNoise', 'f0': 0, 'f1': 0.3}
+netParams.cellParams['pyr']['secs']['soma']['threshold'] = -10 #?? tunable
+
+netParams.cellParams['pyr']['secs']['axon_0']['mechs']['hh'] = {}
+#netParams.cellParams['pyr']['secs']['axon_0']['geom']['Ra'] = 150 #if want same Ra everywhere
+#if want mechanisms like in fleming, tunable
+# netParams.cellParams['pyr']['secs']['axon_0']['mechs']['cortical_axon_i_leak'] = {'g_l': 3.3e-5}#0.02}
+# netParams.cellParams['pyr']['secs']['axon_0']['mechs']['cortical_axon_i_na'] = {'g_Na': 4000e-4}#2800e-4}
+# netParams.cellParams['pyr']['secs']['axon_0']['mechs']['cortical_axon_i_kv'] = {'g_Kv': 20e-4}#5e-4}
+# netParams.cellParams['pyr']['secs']['axon_0']['mechs']['cortical_axon_i_kd'] = {'g_Kd': 0.015}#0.0072}
+
 #%% Create population
-pop_Size=100
+pop_Size=10
+
 netParams.popParams['GPe_pop'] = {
     "cellModel": "",
     "cellType": 'GPe',
@@ -225,6 +254,24 @@ netParams.popParams['Inter_pop'] = {
     # ]
 }
 
+netParams.popParams['Cort_pop'] = {
+    "cellModel": "pyr_model",
+    "cellType": 'pyr_type',
+    "numCells": pop_Size
+    # "yRange": [
+    #     0,
+    #     500
+    # ],
+    # "xRange": [
+    #     500,
+    #     1000
+    # ],
+    # "zRange": [
+    #     0,
+    #     100
+    # ]
+}
+
 #check!! - modify upon healthy state
 netParams.popParams['Str_pop'] = {
     "cellModel": "",
@@ -237,10 +284,11 @@ netParams.popParams['Str_pop'] = {
 
 #%% Add stimulus
 #check out amplitude again!!
-netParams.stimSourceParams['bias_gpe'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.04} #-0.009
-netParams.stimSourceParams['bias_gpi'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.33} #0.006 #0.356
-netParams.stimSourceParams['bias_stn'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0} #-0.125 #0.6
+netParams.stimSourceParams['bias_gpe'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp':-0.009}# 0.04} #-0.009
+netParams.stimSourceParams['bias_gpi'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.006}#0.33} #0.006 #0.356
+netParams.stimSourceParams['bias_stn'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': -0.125}#0} #-0.125 #0.6
 netParams.stimSourceParams['bias_inter'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.070} 
+netParams.stimSourceParams['bias_cort'] = {'type': 'IClamp', 'del': 0, 'dur': 1e12, 'amp': 0.245} 
 #no bias current for thalamus in original paper
 # netParams.stimSourceParams['bkg']={
 #     'type': 'NetStim',
@@ -253,9 +301,11 @@ netParams.stimTargetParams['bias_gpe->gpe'] = {'source': 'bias_gpe','sec':'soma'
 netParams.stimTargetParams['bias_gpi->gpi'] = {'source': 'bias_gpi','sec':'soma', 'loc': 0.5, 'conds': {'pop':'GPi_pop'}}
 netParams.stimTargetParams['bias_stn->stn'] = {'source': 'bias_stn','sec':'soma', 'loc': 0.5, 'conds': {'pop':'STN_pop'}}
 netParams.stimTargetParams['bias_inter->inter'] = {'source': 'bias_inter','sec':'soma', 'loc': 0.5, 'conds': {'pop':'Inter_pop'}}
+netParams.stimTargetParams['bias_cort->cort'] = {'source': 'bias_cort', 'sec':'soma', 'loc': 0.5,'conds': {'pop':'Cort_pop'}}
 
 #%% Synaptic mechanism parameters
 netParams.synMechParams['AMPA'] = {'mod': 'AMPA_S'}  # excitatory synaptic mechanism
+netParams.synMechParams['AMPA_axon'] = {'mod': 'AMPA_S', 'selfNetCon': {'sec': 'axon_0'}}  # excitatory synaptic mechanism
 netParams.synMechParams['GABA'] = {'mod': 'GABAa_S'}  # inhibitory synaptic mechanism
 
 # netParams.stimTargetParams['bkg->Th'] = {
@@ -318,6 +368,7 @@ netParams.connParams['GPe->STN'] = {
     'synMech': 'GABA',
     'delay': 3}
 
+#change weight for parkinsonian state
 netParams.connParams['GPe->GPe'] = {
     'preConds': {'pop': 'GPe_pop'}, 
     'postConds': {'pop': 'GPe_pop'},
@@ -328,6 +379,7 @@ netParams.connParams['GPe->GPe'] = {
     'synMech': 'GABA',
     'delay': 4}
 
+#uncomment for parkinsonian state
 # netParams.connParams['Str->GPe'] = {
 #     'preConds': {'pop': 'Str_pop'}, 
 #     'postConds': {'pop': 'GPe_pop'},
@@ -338,6 +390,58 @@ netParams.connParams['GPe->GPe'] = {
 #     'synMech': 'GABA',
 #     'delay': 1}
 
+netParams.connParams['Th->Cort'] = {
+    'preConds': {'pop': 'Th_pop'}, 
+    'postConds': {'pop': 'Cort_pop'},
+    'weight': 5, 
+    'sec': 'soma',
+    'loc': 0.5,
+    'convergence': 1,
+    'synMech': 'AMPA',
+    'delay': 2}
+
+### why do we need this population? 
+# netParams.connParams['Cort->Th'] = {
+#     'preConds': {'pop': 'Cort_pop'}, 
+#     'postConds': {'pop': 'Th_pop'},
+#     'weight': 0, 
+#     'sec': 'soma',
+#     'loc': 0.5,
+#     'convergence': 1,
+#     'synMech': 'AMPA',
+#     'delay': 2}
+
+netParams.connParams['Inter->Cortsoma'] = {
+    'preConds': {'pop': 'Inter_pop'}, 
+    'postConds': {'pop': 'Cort_pop'},
+    'weight': 'uniform(0,6.0e-3)', 
+    'sec': 'soma',
+    'loc': 0.5,
+    'convergence': 10,
+    'synMech': 'GABA',
+    'delay': 2}
+
+#should change the source in mechanism to axon!!! - did it
+netParams.connParams['Cortaxon->Inter'] = {
+    'preConds': {'pop': 'Cort_pop'}, 
+    'postConds': {'pop': 'Inter_pop'},
+    'weight': 'uniform(0,2.5e-3)', 
+    'sec': 'soma',
+    'loc': 0.5,
+    'convergence': 10,
+    'synMech': 'AMPA_axon',
+    'delay': 2}
+
+netParams.connParams['Cortaxon->STN'] = {
+    'preConds': {'pop': 'Cort_pop'}, 
+    'postConds': {'pop': 'STN_pop'},
+    'weight': 0.12, 
+    'sec': 'soma',
+    'loc': 0.5,
+    'convergence': 5,
+    'synMech': 'AMPA_axon',
+    'delay': 1}
+
 #%% cfg  
 cfg = specs.SimConfig()					            # object of class SimConfig to store simulation configuration
 cfg.duration = 1*1e3 						            # Duration of the simulation, in ms
@@ -347,11 +451,12 @@ cfg.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with tr
 cfg.recordStep = 0.01 			
 cfg.filename = 'model_output'  			# Set file output name
 cfg.saveJson = False
-cfg.analysis['plotTraces'] = {'include': [0,pop_Size,2*pop_Size, 3*pop_Size, 4*pop_Size]} # Plot recorded traces for this list of cells
+cfg.analysis['plotTraces'] = {'include': [0,pop_Size,2*pop_Size, 3*pop_Size, 4*pop_Size, 5*pop_Size]} # Plot recorded traces for this list of cells
 cfg.hParams['celsius'] = 36
 cfg.hParams['v_init'] = -68
 
 #%% run
 sim.createSimulateAnalyze(netParams = netParams, simConfig = cfg)
-sim.analysis.plot2Dnet(include = ['GPe_pop','STN_pop', 'GPi_pop', 'Th_pop', 'Inter_pop']);
+sim.analysis.plot2Dnet(include = ['GPe_pop','STN_pop', 'GPi_pop', 'Th_pop', 'Inter_pop', 'Cort_pop']);
 sim.analysis.plotSpikeStats();
+#sim.analysis.plotShape(includePre=[], includePost=['Cort_pop'], showSyns=False, figSize=(4,9), dist=0.8, showFig=True)
--- mod_func.c
+++ mod_func.c
@@ -3,6 +3,14 @@
 #define IMPORT extern __declspec(dllimport)
 IMPORT int nrnmpi_myid, nrn_nobanner_;
 
+extern void _Cortical_Axon_I_Kd_reg();
+extern void _Cortical_Axon_I_Kv_reg();
+extern void _Cortical_Axon_I_Leak_reg();
+extern void _Cortical_Axon_I_Na_reg();
+extern void _Cortical_Soma_I_K_reg();
+extern void _Cortical_Soma_I_Leak_reg();
+extern void _Cortical_Soma_I_M_reg();
+extern void _Cortical_Soma_I_Na_reg();
 extern void _Destexhe_Static_AMPA_Synapse_reg();
 extern void _Destexhe_Static_GABAA_Synapse_reg();
 extern void _Interneuron_I_K_reg();
@@ -12,7 +20,6 @@ extern void _SynNoise_reg();
 extern void _Thalamic_I_Na_K_reg();
 extern void _Thalamic_I_T_reg();
 extern void _Thalamic_I_leak_reg();
-extern void _izhi2007b_reg();
 extern void _myions_reg();
 extern void _pGPeA_reg();
 extern void _pSTN_reg();
@@ -22,6 +29,14 @@ void modl_reg(){
     if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
 	fprintf(stderr, "Additional mechanisms from files\n");
 
+fprintf(stderr," Cortical_Axon_I_Kd.mod");
+fprintf(stderr," Cortical_Axon_I_Kv.mod");
+fprintf(stderr," Cortical_Axon_I_Leak.mod");
+fprintf(stderr," Cortical_Axon_I_Na.mod");
+fprintf(stderr," Cortical_Soma_I_K.mod");
+fprintf(stderr," Cortical_Soma_I_Leak.mod");
+fprintf(stderr," Cortical_Soma_I_M.mod");
+fprintf(stderr," Cortical_Soma_I_Na.mod");
 fprintf(stderr," Destexhe_Static_AMPA_Synapse.mod");
 fprintf(stderr," Destexhe_Static_GABAA_Synapse.mod");
 fprintf(stderr," Interneuron_I_K.mod");
@@ -31,12 +46,19 @@ fprintf(stderr," SynNoise.mod");
 fprintf(stderr," Thalamic_I_Na_K.mod");
 fprintf(stderr," Thalamic_I_T.mod");
 fprintf(stderr," Thalamic_I_leak.mod");
-fprintf(stderr," izhi2007b.mod");
 fprintf(stderr," myions.mod");
 fprintf(stderr," pGPeA.mod");
 fprintf(stderr," pSTN.mod");
 fprintf(stderr, "\n");
     }
+_Cortical_Axon_I_Kd_reg();
+_Cortical_Axon_I_Kv_reg();
+_Cortical_Axon_I_Leak_reg();
+_Cortical_Axon_I_Na_reg();
+_Cortical_Soma_I_K_reg();
+_Cortical_Soma_I_Leak_reg();
+_Cortical_Soma_I_M_reg();
+_Cortical_Soma_I_Na_reg();
 _Destexhe_Static_AMPA_Synapse_reg();
 _Destexhe_Static_GABAA_Synapse_reg();
 _Interneuron_I_K_reg();
@@ -46,7 +68,6 @@ _SynNoise_reg();
 _Thalamic_I_Na_K_reg();
 _Thalamic_I_T_reg();
 _Thalamic_I_leak_reg();
-_izhi2007b_reg();
 _myions_reg();
 _pGPeA_reg();
 _pSTN_reg();
